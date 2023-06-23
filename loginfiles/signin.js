b3e6d03c1c561a8b6cfb711da8db33c7c1d12c89
stephenstrang <137408930+stephenstrang@users.noreply.github.com>
2023-06-23 00:46:10 -0700

    Add files via upload

---
+++ loginfiles/signin.js
@@ -0,0 +1,23 @@
+$(function () {
+
+
+
+	jQuery.support.placeholder = false;
+
+	test = document.createElement('input');
+
+	if('placeholder' in test) jQuery.support.placeholder = true;
+
+	
+
+	if (!$.support.placeholder) {
+
+		$('.field').find ('label').show ();
+
+		
+
+	}
+
+	
+
+});
